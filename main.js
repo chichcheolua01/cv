7914d3ad235de407c89ed3b12e40e7667acfea6c
Tran Trung Nghia <nghiattph19335@fpt.edu.vn>
2022-06-01 14:55:13 +0700

    update

--- main.js
+++ main.js
@@ -12,8 +12,36 @@ btn_out.addEventListener("click", function (e) {
       menu.classList.add("hidden");
       btn_out.classList.add("hidden");
       btn_menu_div.classList.remove("hidden");
-    } else {
-      menu.classList.remove("hidden");
     }
   }
 });
+
+btn_menu.addEventListener("click", function (e) {
+  e.preventDefault();
+  var count = 1;
+  count += 1;
+  for (var i = 0; i <= count; i++) {
+    menu.classList.remove("hidden");
+    btn_out.classList.remove("hidden");
+    btn_menu_div.classList.add("hidden");
+  }
+});
+
+const images = document.querySelectorAll(".slider");
+var current = 0;
+function changeImage() {
+  setInterval(function () {
+    for (let i = 0; i < images.length; i++) {
+      images[i].style.opacity = 0;
+    }
+    if (current > images.length - 2) {
+      current = 0;
+    } else {
+      current = current + 1;
+    }
+    images[current].style.opacity = 1;
+  }, 2000);
+}
+
+changeImage();
+
